cf839167898b1a7bdf577216bc06bfee4da47a27
m.yermolovich <m.yermolovich@dev1team.net>
2012-05-24 14:08:17 +0300

    Implements "Request" class

--- lib/bank_audi.rb
+++ lib/bank_audi.rb
@@ -1,5 +1,70 @@
 require "bank_audi/version"
+require 'active_support/all'
+require 'yaml'
+require 'cgi'
 
 module BankAudi
-  # Your code goes here...
+  OPTIONS = YAML.load_file('config/bank_audi.yml')
+
+  class Request
+    attr_reader :secret_code, :access_code, :merchant, :url
+    attr_accessor :merchant_txn_ref, :order_info, :amount, :return_url
+    alias :merchant_id :merchant
+
+    def initialize(options = {})
+      @secret_code, @access_code, @merchant, @url =
+        OPTIONS['secret_code'], OPTIONS['access_code'], OPTIONS['merchant'], OPTIONS['url']
+      @merchant_txn_ref, @order_info, @amount, @return_url =
+        options[:merchant_txn_ref], options[:order_info], options[:amount], options[:return_url]
+    end
+
+    def valid?
+      attributes_names.each do |attribute|
+        return false if self.send(attribute).blank?
+      end
+    end
+
+    def invalid?
+      !valid?
+    end
+
+    def full_url
+      return nil if invalid?
+      params = String.new
+      { 'accessCode' => @access_code, 'merchTxnRef' => @merchant_txn_ref, 'merchant' => @merchant,
+        'orderInfo' => @order_info, 'amount' => @amount, 'returnURL' => @return_url }.each do |name, value|
+          params << '&' if params.present?
+          params << (CGI::escape(name) + '=' + CGI::escape(value.to_s))
+        end
+      params << '&vpc_SecureHash=' + vpc_secure_hash.to_s
+      @url + '?' + params
+    end
+
+    def attributes
+      value = {}
+      attributes_names.each do |attribute|
+        value[attribute.to_sym] = self.send(attribute)
+      end
+      value
+    end
+
+    def attributes=(value = {})
+      value.each do |name, attr_value|
+        self.send("#{name}=", attr_value)
+      end
+    end
+
+    private
+      def attributes_names
+        %w(secret_code access_code merchant url merchant_txn_ref order_info amount return_url)
+      end
+
+      def vpc_secure_hash
+        Digest::MD5.hexdigest(@secret_code.to_s + @access_code.to_s + @amount.to_s + @merchant_txn_ref.to_s +
+          @merchant.to_s + @order_info.to_s + @return_url.to_s).upcase
+      end
+  end
+
+  class Responce
+  end
 end
--- spec/bank_audi_spec.rb
+++ spec/bank_audi_spec.rb
@@ -1,5 +1,79 @@
 require 'spec_helper'
 
 describe BankAudi do
-  pending 'first test'
+  describe 'Request' do
+
+    before :each do
+      @request = BankAudi::Request.new
+    end
+
+    def valid(request)
+      request.merchant_txn_ref, request.order_info, request.amount, request.return_url =
+        'merchant-txn-ref', 'order-info', 100, 'http://www.google.com'
+      request
+    end
+
+    it 'should initialize with default attributes' do
+      [:secret_code, :access_code, :merchant, :url].each do |attribute|
+        @request.send(attribute).should_not be_blank
+      end
+    end
+
+    it 'should initialize with options' do
+      request = BankAudi::Request.new :merchant_txn_ref => 'merchant-txn-ref', :order_info => 'order-info',
+        :amount => 100, :return_url => 'http://www.google.com'
+      [:merchant_txn_ref, :order_info, :amount, :return_url].each do |attribute|
+        request.send(attribute).should_not be_blank
+      end
+      request.should be_valid
+    end
+
+    it 'merchant_id is synonym of merchant' do
+      @request.merchant_id.should eq(@request.merchant)
+    end
+
+    it 'should valid' do
+      valid(@request).should be_valid
+    end
+
+    it 'should invalid' do
+      @request.should_not be_valid
+      [:merchant_txn_ref, :order_info, :amount, :return_url].each do |attribute|
+        request = valid(@request)
+        request.send("#{attribute}=", nil)
+        request.should_not be_valid
+      end
+    end
+
+    it 'should return full url' do
+      url = valid(@request).full_url
+      params = CGI::parse(url)
+      params['vpc_SecureHash'].first.should eq('A37A981709D939C0E04B640E26677471')
+    end
+
+    it 'should return attributes (method attributes)' do
+      default_attributes =  {
+        :secret_code => 'XXXXXX', :access_code => 'XXXXXX', :merchant => 'XXXXXX',
+        :url => 'https://gw1.audicards.com/TPGWeb/payment/prepayment.action'
+      }
+
+      @request.attributes.should eq(
+        default_attributes.merge(:merchant_txn_ref => nil, :order_info => nil, :amount => nil,
+          :return_url => nil)
+      )
+
+      valid(@request).attributes.should eq(
+        default_attributes.merge(:merchant_txn_ref => 'merchant-txn-ref', :order_info => 'order-info',
+          :amount => 100, :return_url => 'http://www.google.com')
+      )
+    end
+
+    it 'should set attirubtes (method attributes=)' do
+      @request.attributes = { :merchant_txn_ref => 'merchant-txn-ref', :order_info => 'order-info',
+        :amount => 100, :return_url => 'http://www.google.com' }
+      @request.attributes.should eq(
+          valid(@request).attributes
+      )
+    end
+  end
 end
